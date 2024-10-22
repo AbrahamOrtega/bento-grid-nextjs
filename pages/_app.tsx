b51e38663e34b222a6809076fd73315728821a64
Abraham Ortega <AbrahamOrtegaM@outlook.com>
2024-10-22 13:41:29 -0600

    bento grid desktop

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,16 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import { DM_Sans } from "next/font/google";
+
+const dmsans = DM_Sans({
+  subsets: ["latin"],
+  weight: ["400", "500"],
+});
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <main className={dmsans.className}>
+      <Component {...pageProps} />
+    </main>
+  );
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,115 +1,136 @@
 import Image from "next/image";
-import localFont from "next/font/local";
-
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
-});
 
 export default function Home() {
   return (
-    <div
-      className={`${geistSans.variable} ${geistMono.variable} grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]`}
-    >
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              pages/index.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
+    <>
+      <div className="grid grid-cols-4 grid-rows-12 h-screen w-full gap-[24px] px-32 py-12">
+        <div className="flex flex-col px-[32px] py-[40px] items-center justify-center gap-[24px] col-span-1 row-span-6 bg-[#F9EEE1] rounded-[16px]">
+          <h3 className="text-[24px] font-[600] ">
+            Create and schedule content
+            <span className="text-[#7650DC] italic">quicker.</span>
+          </h3>
+          <div>
             <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
+              src="/images/illustration-create-post.webp"
+              alt=""
+              width={400}
+              height={400}
             />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+          </div>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+
+        <div className="flex flex-col px-[32px] py-[40px] items-center justify-center gap-[24px] col-span-2 row-span-4 bg-[#7650DC] rounded-[16px]">
+          <h2 className="text-[32px] font-[600] text-white text-center">
+            Social Media <span className="text-[#FFCB6B]">10x</span>{" "}
+            <span className="italic">Faster</span> with IA
+          </h2>
+          <div className="flex flex-col w-full items-center gap-y-[16px]">
+            <Image
+              src="/images/illustration-five-stars.webp"
+              alt=""
+              width={200}
+              height={200}
+            />
+            <p className="text-[18px] text-gray-300">
+              Over 4,000 5-star reviews
+            </p>
+          </div>
+        </div>
+
+        <div className="flex flex-col py-[40px] items-center justify-center gap-[24px] col-span-1 row-span-8 bg-[#DACFFC] rounded-[16px] overflow-hidden">
+          <h3 className="text-[24px] px-[32px] font-[600] text-left">
+            Schedule to social media.
+          </h3>
+          <div className="w-full h-full pl-[32px] relative">
+            <Image
+              className="absolute w-full h-full object-cover object-left-top rounded-[16px]"
+              src="/images/illustration-schedule-posts.webp"
+              alt=""
+              width={400}
+              height={400}
+            />
+          </div>
+          <p className="text-gray-800 px-[32px] font-medium">
+            Optimize post timings to publish content at the perfect time for
+            your audience.
+          </p>
+        </div>
+
+        <div className="flex flex-col px-[32px] py-[40px] items-center justify-center gap-[24px] col-span-1 row-span-4 bg-white rounded-[16px]">
+          <div className="flex w-full">
+            <Image
+              src={"/images/illustration-multiple-platforms.webp"}
+              alt="social media platforms"
+              width={1000}
+              height={1000}
+            />
+          </div>
+
+          <h3 className="text-[24px] font-[600]">
+            Manage multiple accounts and platforms.
+          </h3>
+        </div>
+
+        <div className="flex flex-col px-[32px] pt-[40px] items-center justify-center gap-[24px] col-span-1 row-span-4 bg-[#FFCB6B] rounded-[16px]">
+          <h3 className="text-[24px] font-[600]">
+            Maintain a consistent posting schedule.
+          </h3>
+          <div className="flex w-full h-full relative">
+            <Image
+              className="absolute w-full h-full object-cover object-top"
+              src="/images/illustration-consistent-schedule.webp"
+              alt=""
+              width={400}
+              height={400}
+            />
+          </div>
+        </div>
+
+        <div className="flex flex-col px-[32px] py-[40px] items-center justify-center gap-[24px] col-span-1 row-span-6 bg-[#FFCB6B] rounded-[16px]">
+          <h3 className="text-[24px] font-[600]">
+            Write your content using AI.
+          </h3>
+          <div className="flex">
+            <Image
+              className=""
+              src="/images/illustration-ai-content.webp"
+              alt=""
+              width={200}
+              height={200}
+            />
+          </div>
+        </div>
+
+        <div className="flex flex-col px-[32px] py-[40px] items-center justify-center gap-[24px] col-span-1 row-span-4 bg-white rounded-[16px]">
+          <h2 className="text-[40px] font-[600]">
+            {">56%"}{" "}
+            <p className="text-[16px] font-medium">Faster audience growth</p>
+          </h2>
+          <div className="flex h-full">
+            <Image
+              className=""
+              src="/images/illustration-audience-growth.webp"
+              alt=""
+              width={150}
+              height={150}
+            />
+          </div>
+        </div>
+
+        <div className="flex px-[32px] py-[40px] items-center justify-center gap-[24px] col-span-2 row-span-4 bg-[#7650DC] rounded-[16px]">
+          <div className="flex">
+            <Image
+              src="/images/illustration-grow-followers.webp"
+              alt=""
+              width={250}
+              height={250}
+            />
+          </div>
+          <h3 className="text-[24px] font-[600] text-white">
+            Grow followers with non-stop content.
+          </h3>
+        </div>
+      </div>
+    </>
   );
 }
